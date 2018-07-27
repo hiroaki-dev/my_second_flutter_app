f8ff0fdf099b7eb24ba989ce96e15c01524258f6
Yuki Nakakura <hiroaki.dev@gmail.com>
2018-07-27 23:19:50 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# my_second_flutter_app
+
+Flutter get started.
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ android/app/src/main/kotlin/me/hiroaki/mysecondflutterapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package me.hiroaki.mysecondflutterapp
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity(): FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
